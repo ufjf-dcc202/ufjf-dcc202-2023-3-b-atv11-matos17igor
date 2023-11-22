b552cc77228e0dce3fcd0d47e6f9355a52e81a50
Igor Matos <igorm17_pvp@hotmail.com>
2023-11-21 22:59:32 -0300

    adiciona item no estado

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { getLista } from "./src/lista";
+import { adicionaNaLista, getLista } from "./src/lista.js";
 
 const pEntrada = document.querySelector('#entrada');
 const btnAdicionar = document.querySelector('#adicionar');
@@ -7,8 +7,18 @@ const olItens = document.querySelector('#itens');
 
 atualizarListaOrdenada();
 
+btnAdicionar.addEventListener('click', adicionaItemDaEntrada);
+
+function adicionaItemDaEntrada(){
+    const valor = pEntrada.textContent;
+    adicionaNaLista(valor);
+    pEntrada.textContent = "";
+    atualizarListaOrdenada();
+}
+
 function atualizarListaOrdenada(){
     const lista = getLista();
+    olItens.innerHTML = "";
     for(let i=0; i<lista.length; i++){
         adicionaElementoNaListaOrdenada(lista[i]);
     }

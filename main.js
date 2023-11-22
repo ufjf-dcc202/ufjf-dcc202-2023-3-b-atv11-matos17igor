1951dc707e090af4d0138f2e644e28bbb133e86a
Igor Matos <igorm17_pvp@hotmail.com>
2023-11-21 22:19:31 -0300

    cria tres lis

--- main.js
+++ main.js
@@ -8,3 +8,11 @@ const li = document.createElement("li");
 li.textContent = "Primeiro";
 console.log(li);
 olItens.appendChild(li);
+
+const li2 = document.createElement("li");
+li2.textContent = "Segundo";
+olItens.appendChild(li2);
+
+const li3 = document.createElement("li");
+li3.textContent = "Terceiro";
+olItens.appendChild(li3);
